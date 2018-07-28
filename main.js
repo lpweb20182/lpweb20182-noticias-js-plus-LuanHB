0cbfd163777b70a06fbc309c5fdb037ee38454e9
LuanHB <35976956+LuanHB@users.noreply.github.com>
2018-07-27 23:50:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,28 +6,61 @@ function atualizarLista(noticia) {
     li.setAttribute('id', 'noticia-' + noticia.id);
     li.setAttribute('class', 'noticia');
     li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
-        + noticia.titulo
+        + noticia.titulo 
         + '</p>'
         + '<p class="conteudo">'
+        + noticia.nome
+        + '<br>'
+        + noticia.email
+        + '<br>'
+        + noticia.data
+        +  '<br>'
+        + noticia.hora
+        + '<br>'
         + noticia.conteudo
         + '<br>'
         + '<span>------------------</span>'
         + '<br>'
         + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
-        + '</p>';
-    lista.appendChild(li);
+        + '</p>';  
+    lista.appendChild(li);  
 }
 
 function salvar(form) {
     var titulo = document.getElementById('frm-titulo').value;
+    var nome = document.getElementById('frm-nome').value;
+    var email = document.getElementById('frm-email').value;
+    var data = document.getElementById('frm-data').value;
+    var hora = document.getElementById('frm-hora').value;
     var conteudo = document.getElementById('frm-conteudo').value;
     var noticia = {
         id: noticias.length,
         titulo: titulo,
+		nome: nome,
+		email: email,
+		data: data,
+		hora: hora,
         conteudo: conteudo
     };
     noticias.push(noticia);
-    atualizarLista(noticia);
+
+    var dataNoticia = noticia.data;
+    var data = dataNoticia.split("-");
+    var anoN = parseInt(data[0])
+    var mesN = parseInt(data[1])
+    var diaN = parseInt(data[2])
+    var novaDataNoticia = anoN + mesN + diaN
+
+    var hoje = new Date()
+    var anoH = hoje.getFullYear()
+    var mesH = hoje.getMonth()
+    var diaH = hoje.getDate();
+    var novoHoje = anoH + mesH + diaH + 1
+    
+    if(noticia.data != "" && novaDataNoticia <= novoHoje){
+            atualizarLista(noticia);
+        }
+
     form.reset();
 }
 
